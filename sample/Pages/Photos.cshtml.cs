920a4ae81ae2dc66a507bca05dad194ed9097e23
admin <cinqapps@gmail.com>
2020-01-12 00:28:16 +0530

    Project added.

---
+++ sample/Pages/Photos.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GalleryApp.Pages
+{
+    public class PhotosModel : PageModel
+    {
+        public List<string> FileNames { get; set; }
+        public void OnGet()
+        {
+            FileNames = new List<string>();
+
+            var limit = 10;
+            for (int i = 1; i <= limit; i++)
+            {
+                FileNames.Add(i + ".jpg");
+            }
+        }
+    }
+}
---
+++ sample/Startup.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ImageThumbnail.AspNetCore.Middleware;
+
+
+namespace ImageThumbnail
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            //Gallery - folder containing images
+            //.thumbs - name of thumbnail cahce folder. 
+            ImageThumbnailOptions options = new ImageThumbnailOptions("Gallery", ".thumbs");
+            options.ImageQuality = 20L;
+
+            app.UseImageThumbnail(options);
+
+            app.UseMvc();
+        }
+    }
+}
